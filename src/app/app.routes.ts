ceda0a735a1eabcd366a95b86249c37f68899601
Daniel Cruz <daniel.cruz@ambarseguros.mx>
2025-12-04 13:11:00 -0600

    AuthGuard implemented

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,34 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './guards/auth.guard';
+import { guestGuard } from './guards/guest.guard';
+
 import { HomeView } from '@views/home/home.view';
 import { LoginView } from '@views/login/login.view';
 import { RegisterView } from '@views/register/register.view';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'home', component: HomeView },
-  { path: 'login', component: LoginView },
-  { path: 'register', component: RegisterView },
-  { path: '**', redirectTo: 'login' },
+  {
+    path: 'login',
+    loadComponent: () => import('@views/login/login.view').then((m) => m.LoginView),
+    canActivate: [guestGuard],
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('@views/register/register.view').then((m) => m.RegisterView),
+    canActivate: [guestGuard],
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('@views/home/home.view').then((m) => m.HomeView),
+    canActivate: [authGuard],
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: 'login',
+  },
 ];
